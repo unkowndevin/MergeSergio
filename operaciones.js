382cec26833352a2d7093f5a4a3ef8b880f51075
gabo <gabrosblack8@gmail.com>
2018-05-28 16:53:18 -0500

    Primer script de suma

---
+++ operaciones.js
@@ -0,0 +1,13 @@
+function suma(){
+	var num1=Input.getElementById('num1').values;
+	var num2=Input.getElementById('num2').values;
+	var num3=num1+num2;
+
+	
+}
+
+function resta(){
+	var num1=Input.getElementById('num1').values;
+	var num2=Input.getElementById('num2').values;
+	var num3=num1-num2;
+}
